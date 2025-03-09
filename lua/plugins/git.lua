ff7df4358ed356196c9954e6d39cba9dbc70cc34
nishaaaanth <nishanthkiran2@gmail.com>
2025-03-10 02:10:02 +0530

    feat: update git keybindings

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -11,9 +11,10 @@ return {
     {
         "tpope/vim-fugitive",
         config = function()
-            vim.keymap.set('n', "<leader>ga", ":Git add .<CR>", { desc = "[G]it [A]dd" })
-            vim.keymap.set('n', "<leader>g", ":Git commit<CR>", { desc = "[G]it [C]ommit" })
-            vim.keymap.set('n', "<leader>g", ":Git blame<CR>", { desc = "[G]it [B]lame" })
+            vim.keymap.set('n', "<leader>g", "<CMD>Git<CR>", { desc = "[G]it" })
+            vim.keymap.set('n', "<leader>ga", "<CMD>Git add .<CR>", { desc = "[G]it [A]dd" })
+            vim.keymap.set('n', "<leader>gc", "<CMD>Git commit<CR>", { desc = "[G]it [C]ommit" })
+            vim.keymap.set('n', "<leader>gb", "<CMD>Git blame<CR>", { desc = "[G]it [B]lame" })
         end
     }
 }
